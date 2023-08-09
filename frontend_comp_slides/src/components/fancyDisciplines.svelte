cdb0e456eb85496a874b89132e081ef742ece3d4
Matheus <mt.schreiber01@gmail.com>
2023-08-09 11:54:42 -0300

    :recycle: fancy component connection to backend

--- frontend_comp_slides/src/components/fancyDisciplines.svelte
+++ frontend_comp_slides/src/components/fancyDisciplines.svelte
@@ -1,53 +1,60 @@
-<script lang='ts'>
+<script lang="ts">
   import { onMount } from "svelte";
+  import { api } from "../services/api";
 
-  let disciplines = ['Estrutura de Dados II', 'Eletromagnetismo I', 'Circuitos Elétricos I', 'Circuitos Lógicos', 'Programação II', 'Fisica III']
-  
-  let text = ""
+  let disciplines: string[] = [""];
 
-  let duration = 3000
+  let text = "";
 
-  function sleep(ms:number) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+  let duration = 3000;
+
+  function sleep(ms: number) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
   }
 
-  async function addTyping(string: string){
-    let i=0
-    let timePerLetter = (duration*.45)/string.length // this is to ensure 2sec for all words
-    while(i<string.length) {
-      text += string[i++]
-      await sleep(timePerLetter)
+  async function addTyping(string: string) {
+    let i = 0;
+    let timePerLetter = (duration * 0.45) / string.length; // this is to ensure 2sec for all words
+    while (i < string.length) {
+      text += string[i++];
+      await sleep(timePerLetter);
     }
   }
 
-  async function eraseTyping(string: string){
-    let i=string.length
-    let timePerLetter = duration*.10/string.length // this is to ensure 2sec for all words
-    while(i-->0) {
-      text = text.slice(0,-1)
-      await sleep(timePerLetter)
+  async function eraseTyping(string: string) {
+    let i = string.length;
+    let timePerLetter = (duration * 0.1) / string.length; // this is to ensure 2sec for all words
+    while (i-- > 0) {
+      text = text.slice(0, -1);
+      await sleep(timePerLetter);
     }
   }
 
-  onMount(async()=>{
-    while(true){
-      let index = Math.round(Math.random()*(disciplines.length-1))
-      console.log(index)
-      await addTyping(disciplines[index])
-      await sleep(duration*.45)
-      await eraseTyping(disciplines[index])
+  async function fetchDisciplines() {
+    const response = await api.get("disciplines");
+    response.data.map((discipline: string) =>
+      disciplines.push(discipline.split("|")[1])
+    );
+  }
+
+  onMount(async () => {
+    fetchDisciplines();
+
+    while (true) {
+      let index = Math.floor(Math.random() * disciplines.length);
+      console.log(disciplines[index]);
+      await addTyping(disciplines[index]);
+      await sleep(duration * 0.45);
+      await eraseTyping(disciplines[index]);
     }
-  })
+  });
 
-  let blink = true
-  setInterval(()=>{
-    blink=!blink
-  }, 200)
-
-
+  let blink = true;
+  setInterval(() => {
+    blink = !blink;
+  }, 200);
 </script>
 
-
 <h1 id="title-typing-effect" class="text-6xl font-bold text-terciary w-[400px]">
-  {text}<span class="text-green {blink?"invisible":"visible"}">_</span>
+  {text}<span class="text-green {blink ? 'invisible' : 'visible'}">_</span>
 </h1>

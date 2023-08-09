caf7e34085bc3b70fa439dee2303aeb08b54583a
Matheus <mt.schreiber01@gmail.com>
2023-08-09 11:36:44 -0300

    :package: add cors

--- backend_comp_slides/backend_comp_slides/settings.py
+++ backend_comp_slides/backend_comp_slides/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
 
     "rest_framework",
     "base.apps.BaseConfig",
+    "corsheaders",
 ]
 
 AUTH_USER_MODEL = "base.User"
@@ -40,6 +41,7 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -127,3 +129,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ORIGIN_ALLOW_ALL = True

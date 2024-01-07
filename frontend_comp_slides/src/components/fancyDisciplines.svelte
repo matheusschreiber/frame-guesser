fddff11247eeed14cb0ab4930b6c3f535a557c8a
Matheus <mt.schreiber01@gmail.com>
2024-01-07 18:48:45 -0300

    :art: ui changes

--- frontend_comp_slides/src/components/fancyDisciplines.svelte
+++ frontend_comp_slides/src/components/fancyDisciplines.svelte
@@ -2,7 +2,7 @@
   import { onMount } from "svelte";
   import { api } from "../services/api";
 
-  let disciplines: string[] = [""];
+  let disciplines: string[] = [];
 
   let text = "";
 
@@ -41,10 +41,15 @@
     fetchDisciplines();
 
     while (true) {
-      let index = Math.floor(Math.random() * disciplines.length);
-      await addTyping(disciplines[index]);
-      await sleep(duration * 0.45);
-      await eraseTyping(disciplines[index]);
+      if (disciplines.length) {
+        let index = Math.floor(Math.random() * disciplines.length);
+        
+        await addTyping(disciplines[index]);
+        await sleep(duration * 0.45);
+        await eraseTyping(disciplines[index]);
+      }
+
+        await sleep(0.01);
     }
   });
 
@@ -54,6 +59,6 @@
   }, 200);
 </script>
 
-<h1 id="title-typing-effect" class="text-6xl font-bold text-terciary w-[400px]">
+<h1 id="title-typing-effect" class="text-5xl font-bold text-terciary w-[400px]">
   {text}<span class="text-green {blink ? 'invisible' : 'visible'}">_</span>
 </h1>
--- frontend_comp_slides/src/components/footer.svelte
+++ frontend_comp_slides/src/components/footer.svelte
@@ -3,6 +3,6 @@
     <a href="https://github.com/matheusschreiber" target="_blank"><img class="w-8 h-8 hover:scale-105 transition-transform" src="icons/git.svg" alt="git icon"/></a>
     <a href="https://www.linkedin.com/in/matheus-schreiber-49472321a/" target="_blank"><img class="w-8 h-8 hover:scale-105 transition-transform" src="icons/linkedin.svg" alt="linkedin icon"/></a>
     <a href="https://matheus-schreiber.com.br" target="_blank"><img class="w-8 h-8 hover:scale-105 transition-transform" src="icons/globe.svg" alt="globe icon"/></a>
-    <p>2023・<span class="font-bold">Matheus Meier Schreiber</span>・Engenharia da Computação・UFES</p>
+    <p>2023・<span class="font-bold">Comp Slides</span>・Engenharia da Computação・UFES</p>
   </div>
 </div>

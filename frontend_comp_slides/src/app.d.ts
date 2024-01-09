af7bc83fa5e28814523379c04cf8dfc7010f5262
Matheus <mt.schreiber01@gmail.com>
2024-01-08 21:51:37 -0300

    :wastebasket: cleaning old code

--- frontend_comp_slides/src/app.d.ts
+++ frontend_comp_slides/src/app.d.ts
@@ -1,16 +1,8 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
-	type User = {
-		id: number
-		username: string
-	}
-
 	namespace App {
 		// interface Error {}
-		interface Locals {
-			user: User | null
-		}
 		// interface Locals {}
 		// interface PageData {}
 		// interface Platform {}
--- frontend_comp_slides/src/lib/index.ts
+++
@@ -1,8 +0,0 @@
-import { redirect, type RequestEvent } from "@sveltejs/kit"
-import { getCookie } from "../services/cookies"
-
-export const authenticateUser = () => {
-	const rawCookie = getCookie("auth") // FIXME: nao tem document.cookie em SSR
-	if (!rawCookie) return false
-	return JSON.parse(rawCookie).access
-}

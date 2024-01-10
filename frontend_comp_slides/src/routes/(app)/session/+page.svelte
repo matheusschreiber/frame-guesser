b8bd09fa0e56cf9d880b2b8e58f1d2aee7478377
Matheus <mt.schreiber01@gmail.com>
2024-01-09 22:32:13 -0300

    :recycle: adjusting for new backend models

--- frontend_comp_slides/src/routes/(app)/session/+page.svelte
+++ frontend_comp_slides/src/routes/(app)/session/+page.svelte
@@ -6,19 +6,19 @@
   import { api } from "../../../services/api";
   import Loading from "../../../components/loading.svelte";
   import { getCookie, setCookie } from "../../../services/cookies";
+    import Error from "../../+error.svelte";
 
   let confirm = false;
   let selected: number | null = null;
-  let hintsAmount = 0;
   let hintsUsed = 0;
+  let totalHintsAmount = 0;
 
-  let currentSlide = 1;
-  let slidesAmount = 5;
+  let slidesLeftAmount: number;
   let difficultyLevel: number;
 
   var loading = true;
   var hasAnswered = false;
-  var answer: number | null = null; 
+  var answer: number | null = null;
   var loadingHint = false;
 
   var options = [
@@ -27,14 +27,14 @@
     "Carregando... | Carregando...",
     "Carregando... | Carregando...",
   ];
-  var slideImage:string  | undefined;
+  var slideImage: string | undefined;
 
   function handleConfirm() {
     if (hasAnswered) return;
 
     if (confirm) {
       fetchNewHint();
-      if (hintsUsed + 1 <= hintsAmount) hintsUsed++;
+      if (hintsUsed < totalHintsAmount) hintsUsed++;
       confirm = false;
     } else confirm = true;
   }
@@ -49,7 +49,9 @@
   async function handleAnswerSlide() {
     let runId = getCookie("runId");
     if (!runId) return;
-    const response = await api.put("slide/answer/" + runId, { answer: options[selected ? selected : 0], });
+    const response = await api.put("slide/answer/" + runId, {
+      answer: options[selected ? selected : 0],
+    });
 
     if (response.data.answer == true) {
       Swal.fire({
@@ -69,11 +71,12 @@
       Swal.fire("Uai", "Houve algum problema com os servidores", "error").then(
         () => {
           selected = null;
-        }
+        },
       );
     }
 
-    slideImage = import.meta.env.VITE_API_URL + '/' + response.data.slide_image_path;
+    slideImage =
+      import.meta.env.VITE_API_URL + "/" + response.data.slide_image_path;
 
     options.map((option: string, idx: number) => {
       if (option.toLowerCase() == response.data.slide.toLowerCase())
@@ -87,89 +90,109 @@
   async function handleNextSlide() {
     loading = true;
 
-    if (!hasAnswered) handleAnswerSlide()
-    else {
-      await fetchSlide()
-      currentSlide++;
+    if (!hasAnswered) {
+      handleAnswerSlide();
+    } else {
+      await fetchSlide();
     }
   }
 
   async function fetchSlide() {
-    if (currentSlide == slidesAmount) goto(`/results`, { replaceState: false });
+    if (slidesLeftAmount = 0) goto(`/results`, { replaceState: false });
 
     hasAnswered = false;
     loading = true;
     answer = null;
     selected = null;
     hintsUsed = 0;
-    
+
     let response;
     let currentRun = getCookie("runId");
 
     try {
-      if (currentRun && currentRun!="undefined"){
-        response = await api.get("slide/random/"+currentRun);
+      if (currentRun && currentRun != "undefined") {
+        response = await api.get("slide/random/" + currentRun);
       } else {
         response = await api.get("slide/random");
       }
-    } catch(err) {
-      Swal.fire("Uai", "Houve algum problema com os servidores", "error")
+    } catch (err:any) {
+      if (err.response.status === 301) {
+        goto(`/results`);
+      } else {
+        Swal.fire("Uai", "Houve algum problema com os servidores", "error");
+      }
+      
       return;
     }
 
-    slideImage = import.meta.env.VITE_API_URL + '/' +response.data.slide_image_path;
-    hintsAmount = response.data.hints_amount - 1;
+    slideImage =
+      import.meta.env.VITE_API_URL + "/" + response.data.slide_image_path;
+
     setCookie("runId", response.data.run_id);
-    slidesAmount = response.data.slides_left_amount
     loading = false;
-    difficultyLevel = response.data.difficulty_level
-    options = response.data.alternatives
+    difficultyLevel = response.data.difficulty_level;
+    options = response.data.slide_alternatives;
+    hintsUsed = response.data.hints_used;
+    slidesLeftAmount = response.data.slides_left_amount;
+    totalHintsAmount = response.data.hints_total;
+    hasAnswered = hintsUsed == totalHintsAmount ? true : false;
   }
 
   async function fetchNewHint() {
-    loadingHint = true
+    loadingHint = true;
     let runId = getCookie("runId");
     if (!runId) {
-      Swal.fire("Uai", "Houve algum problema com a sua sessão, redirecionando...", "error")
+      Swal.fire(
+        "Uai",
+        "Houve algum problema com a sua sessão, redirecionando...",
+        "error",
+      );
       return;
     }
 
     const response = await api.post("slide/hint/" + runId);
 
-    slideImage = import.meta.env.VITE_API_URL + '/' +response.data.slide_image_path;
-    loadingHint = false
+    slideImage =
+      import.meta.env.VITE_API_URL + "/" + response.data.slide_image_path;
+    loadingHint = false;
   }
 
-  function clickOutside(element:any, callbackFunction:Function) {
-		function onClickBody(event:Event) {
-			if (!element.contains(event.target)) {
-				callbackFunction();
-			}
-		}
-		document.body.addEventListener('click', onClickBody);
-		return {
-			update(newCallbackFunction:Function) {callbackFunction = newCallbackFunction;},
-			destroy() {document.body.removeEventListener('click', onClickBody);}
-		}
-	}
+  function clickOutside(element: any, callbackFunction: Function) {
+    function onClickBody(event: Event) {
+      if (!element.contains(event.target)) {
+        callbackFunction();
+      }
+    }
+    document.body.addEventListener("click", onClickBody);
+    return {
+      update(newCallbackFunction: Function) {
+        callbackFunction = newCallbackFunction;
+      },
+      destroy() {
+        document.body.removeEventListener("click", onClickBody);
+      },
+    };
+  }
 
   onMount(() => {
     fetchSlide();
 
     // script to automatically scroll to the main content of the page
-    let d = document.getElementById('div-scroll-main')
-    if (d){
+    let d = document.getElementById("div-scroll-main");
+    if (d) {
       window.scrollTo({
-        top: d.offsetTop-25,
+        top: d.offsetTop - 25,
         left: 0,
         behavior: "smooth",
-      })
+      });
     }
   });
 </script>
 
 <main>
-  <section class="my-8 pt-12 pb-24 bg-purple w-fit px-32 m-auto rounded-xl shadow-medium text-center overflow-hidden flex flex-col items-center justify-center">
+  <section
+    class="my-8 pt-12 pb-24 bg-purple w-fit px-32 m-auto rounded-xl shadow-medium text-center overflow-hidden flex flex-col items-center justify-center"
+  >
     <LineBackground variant={3} />
 
     <div id="div-scroll-main">
@@ -193,22 +216,25 @@
       <aside class="w-[600px] flex flex-col justify-end">
         {#if slideImage != ""}
           <div class="flex w-[90%] mx-auto justify-start mb-[-10px]">
-            <h3 class="mt-4 text-whitish bg-terciary w-fit px-2 py-1 pb-3 rounded-lg text-sm">
-              {currentSlide}/{slidesAmount}
+            <h3
+              class="mt-4 text-whitish bg-terciary w-fit px-2 py-1 pb-3 rounded-lg text-sm"
+            >
+              {10-slidesLeftAmount}/{10}
             </h3>
           </div>
         {/if}
-    
+
         {#if slideImage == ""}
           <Loading />
         {:else}
           <img class="rounded-lg h-[400px]" src={slideImage} alt="slide" />
         {/if}
-        
       </aside>
-  
+
       <aside class="flex flex-col items-center justify-end px-6">
-        <div class="flex flex-col gap-4 justify-between pb-8 items-center h-[350px]">
+        <div
+          class="flex flex-col gap-4 justify-between pb-8 items-center h-[350px]"
+        >
           {#each options as item, i}
             <div
               on:click={() => handleSelection(i)}
@@ -219,10 +245,13 @@
               {answer == i
                 ? 'border-green'
                 : selected == i
-                ? hasAnswered
-                  ? 'border-red'
-                  : 'border-pink'
-                : hasAnswered ? 'opacity-[.3] border-secondary cursor-not-allowed':'border-secondary'}">
+                  ? hasAnswered
+                    ? 'border-red'
+                    : 'border-pink'
+                  : hasAnswered
+                    ? 'opacity-[.3] border-secondary cursor-not-allowed'
+                    : 'border-secondary'}"
+            >
               <h3 class="text-whitish font-bold text-sm">
                 {item.split("|")[0].trim()}
               </h3>
@@ -230,29 +259,34 @@
             </div>
           {/each}
         </div>
-    
+
         <div class="flex justify-around w-full h-[50px]">
           <div class="flex flex-col items-center">
             {#if loadingHint}
               <Loading />
             {:else}
               {#if hintsUsed > 0}
-                <p class="absolute font-fredoka text-sm text-whitish mt-[-25px]">
-                  {hintsUsed}/{hintsAmount}
+                <p
+                  class="absolute font-fredoka text-sm text-whitish mt-[-25px]"
+                >
+                  {hintsUsed}/{totalHintsAmount}
                 </p>
               {/if}
               <div
-                use:clickOutside={() => confirm = false}
-                on:click={hintsUsed != hintsAmount ? () => handleConfirm() : null}
+                use:clickOutside={() => (confirm = false)}
+                on:click={hintsUsed != totalHintsAmount
+                  ? () => handleConfirm()
+                  : null}
                 role="button"
                 tabindex={1}
                 class="bg-secondary rounded-3xl shadow-medium h-12 w-12 flex
                 justify-center items-center border-2 select-none
                 {confirm ? 'border-yellow' : 'border-secondary'}
-                {hintsUsed == hintsAmount || hasAnswered
+                {hintsUsed == totalHintsAmount || hasAnswered
                   ? 'opacity-20 cursor-not-allowed'
                   : 'opacity-100 cursor-pointer'}"
-                on:keypress={() => {}}>
+                on:keypress={() => {}}
+              >
                 <img
                   src="icons/lamp.svg"
                   alt="icon lamp"
@@ -273,7 +307,7 @@
               {/if}
             {/if}
           </div>
-    
+
           {#if loading}
             <Loading />
           {:else}
@@ -281,8 +315,11 @@
               class="px-4 py-2 h-12 bg-terciary font-bold text-sm rounded-lg border-2
             {selected == null
                 ? 'border-terciary text-whitish'
-                : answer == null ? 'border-pink text-pink' 
-                : selected == answer ? 'border-green text-green' : 'border-red text-red'}"
+                : answer == null
+                  ? 'border-pink text-pink'
+                  : selected == answer
+                    ? 'border-green text-green'
+                    : 'border-red text-red'}"
               on:click={loading ? null : () => handleNextSlide()}
             >
               {hasAnswered ? "AVANÇAR" : "VERIFICAR"}
@@ -291,6 +328,5 @@
         </div>
       </aside>
     </div>
-
   </section>
 </main>

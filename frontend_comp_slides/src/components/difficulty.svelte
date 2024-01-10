e57cd296269fbfc5a5347f54c3e2f8c27c777789
Matheus <mt.schreiber01@gmail.com>
2024-01-10 00:12:00 -0300

    :recycle: add component

---
+++ frontend_comp_slides/src/components/difficulty.svelte
@@ -0,0 +1,15 @@
+<script lang='ts'>
+    export let difficultyLevel: number;
+</script>
+
+{#if difficultyLevel === 5}
+    <span class="text-red">NÍVEL DIFÍCIL</span>
+{:else if difficultyLevel === 4}
+    <span class="text-orange">NÍVEL QUASE DIFÍCIL</span>
+{:else if difficultyLevel === 3}
+    <span class="text-yellow">NÍVEL NORMAL</span>
+{:else if difficultyLevel === 2}
+    <span class="text-green">NÍVEL FÁCIL</span>
+{:else if difficultyLevel === 1}
+    <span class="text-blue">NÍVEL MUITO FÁCIL</span>
+{/if}

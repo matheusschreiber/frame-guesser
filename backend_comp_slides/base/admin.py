8907d2b12b444452cd2a37b37a685a756e47d557
Matheus Schreiber <mt.schreiber01@gmail.com>
2025-02-16 23:08:23 -0300

    :recycle: adjusting temporary django admin panels

--- backend_comp_slides/base/admin.py
+++ backend_comp_slides/base/admin.py
@@ -2,6 +2,19 @@ from django.contrib import admin
 
 from .models import *
 
+class RunCustomAdmin(admin.ModelAdmin):
+    list_display = [
+        'user',
+        'current_hint',
+        'slides_left',
+        'total_points',
+    ]
+
+    search_fields = [
+        'user__username',
+        'user__email',
+    ]
+
 class SlidedRunCustomAdmin(admin.ModelAdmin):
     list_display = [
         'id',
@@ -22,16 +35,34 @@ class SlidedRunCustomAdmin(admin.ModelAdmin):
     def difficulty(self, obj):
         return obj.original_slide.difficulty_level
     
+    search_fields = [
+        'run_id__user__username',
+        'original_slide__prof_discipline',
+        'original_slide__difficulty_level'
+    ]
+    
 class ConfigCustomAdmin(admin.ModelAdmin):
     list_display = [
         'name',
         'value'
     ]
 
+class UserCustomAdmin(admin.ModelAdmin):
+    search_fields = [
+        'username',
+        'email'
+    ]
+
+class MessagesCustomAdmin(admin.ModelAdmin):
+    search_fields = [
+        'user__username',
+        'text'
+    ]
+
 admin.site.register(Slide)
-admin.site.register(User)
+admin.site.register(User, UserCustomAdmin)
 admin.site.register(SlideImage)
-admin.site.register(Run)
-admin.site.register(Message)
+admin.site.register(Run, RunCustomAdmin)
+admin.site.register(Message, MessagesCustomAdmin)
 admin.site.register(Config, ConfigCustomAdmin)
 admin.site.register(SlideRun, SlidedRunCustomAdmin)

82f06c5eeef0f2cf07d6b24be62d3a7c2f8c799c
Matheus Schreiber <mt.schreiber01@gmail.com>
2025-06-22 15:00:56 -0300

    :bug: fix bug on static

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 DEBUG = os.getenv("DJANGO_DEBUG_MODE") == "True"
 
-ALLOWED_HOSTS = [os.getenv("ALLOWED_HOSTS"), 'localhost']
+ALLOWED_HOSTS = [os.getenv("ALLOWED_HOSTS"), 'localhost', '127.0.0.1']
 
 # Application definition
 INSTALLED_APPS = [
@@ -186,13 +186,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
 
-MEDIA_URL = "/images/"
-
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

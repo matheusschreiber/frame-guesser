2a43f4a382923a5d44de2763b5d2fad1b0d65550
Matheus Schreiber <matheusmeier.sch2341@gmail.com>
2026-03-04 22:57:21 +0100

    :bug: fix crawler

--- backend/base/api/serializers.py
+++ backend/base/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework.serializers import ModelSerializer
-from base.models import *
+from base.models import Message, Run, Slide, SlideRun, User
 
 class UserSerializer(ModelSerializer):
     class Meta:
